301b9b168fb3cf307dedbb9fe0bb833e142629b2
John B <john@j81.nl>
2019-09-06 14:51:26 +0200

    Update Unifi_Controller.sh

--- JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install ca-certificates apt-transport-https
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install unifi -y
+echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg 
+sudo apt update && sudo apt-get install unifi -y
 sudo apt-get install git -y
 sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
 echo "echo \"\"" >> /etc/profile
