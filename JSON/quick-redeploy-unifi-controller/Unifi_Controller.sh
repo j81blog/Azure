7b5d55ebc2748391fc2cf4d56c684509c1c886d6
John B <john@j81.nl>
2019-09-06 13:31:28 +0200

    Update Unifi_Controller.sh

--- JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update
+sudo apt update && sudo apt upgrade
 sudo apt-get install unifi -y
+sudo apt-get install git -y
+sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
 echo "echo \"\"" >> /etc/profile
 echo "echo \"Your management URL for your Unifi Controller is https://\`curl -s ipecho.net/plain; echo\`:8443\"" >> /etc/profile
 echo "echo \"\"" >> /etc/profile
