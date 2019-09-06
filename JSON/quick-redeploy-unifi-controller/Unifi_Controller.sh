41ae7c64832e9133438f8f1419dd09caef60b2d4
John B <john@j81.nl>
2019-09-06 14:35:42 +0200

    Update Unifi_Controller.sh

--- JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt-get install unifi -y
 sudo apt-get install git -y
 sudo git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
