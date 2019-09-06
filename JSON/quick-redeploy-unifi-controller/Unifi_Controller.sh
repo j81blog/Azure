5dce33b743c6174da45b5c8370df005d6f561de4
John B <john@j81.nl>
2019-09-06 11:54:40 +0200

    Update Unifi_Controller.sh

--- JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list.d/100-ubnt.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
 sudo apt-get install unifi -y
-echo "echo \"\"" >> /etc/profile
-echo "echo \"██▀███  ▓█████ ▓█████▄ ▓█████  ██▓███   ██▓     ▒█████  ▓██   ██▓\"" >> /etc/profile
-echo "echo \"▓██ ▒ ██▒▓█   ▀ ▒██▀ ██▌▓█   ▀ ▓██░  ██▒▓██▒    ▒██▒  ██▒ ▒██  ██▒\"" >> /etc/profile
-echo "echo \"▓██ ░▄█ ▒▒███   ░██   █▌▒███   ▓██░ ██▓▒▒██░    ▒██░  ██▒  ▒██ ██░\"" >> /etc/profile
-echo "echo \"▒██▀▀█▄  ▒▓█  ▄ ░▓█▄   ▌▒▓█  ▄ ▒██▄█▓▒ ▒▒██░    ▒██   ██░  ░ ▐██▓░\"" >> /etc/profile
-echo "echo \"░██▓ ▒██▒░▒████▒░▒████▓ ░▒████▒▒██▒ ░  ░░██████▒░ ████▓▒░  ░ ██▒▓░\"" >> /etc/profile
-echo "echo \"░ ▒▓ ░▒▓░░░ ▒░ ░ ▒▒▓  ▒ ░░ ▒░ ░▒▓▒░ ░  ░░ ▒░▓  ░░ ▒░▒░▒░    ██▒▒▒\"" >> /etc/profile
-echo "echo \" ░▒ ░ ▒░ ░ ░  ░ ░ ▒  ▒  ░ ░  ░░▒ ░     ░ ░ ▒  ░  ░ ▒ ▒░  ▓██ ░▒░\"" >> /etc/profile
-echo "echo \" ░░   ░    ░    ░ ░  ░    ░   ░░         ░ ░   ░ ░ ░ ▒   ▒ ▒ ░░\"" >> /etc/profile
-echo "echo \"  ░        ░  ░   ░       ░  ░             ░  ░    ░ ░   ░ ░\"" >> /etc/profile
-echo "echo \"                ░                                        ░ ░\"" >> /etc/profile
-echo "echo \"                                                   redeploy.se\"" >> /etc/profile
 echo "echo \"\"" >> /etc/profile
 echo "echo \"Your management URL for your Unifi Controller is https://\`curl -s ipecho.net/plain; echo\`:8443\"" >> /etc/profile
 echo "echo \"\"" >> /etc/profile
